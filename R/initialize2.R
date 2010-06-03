1ef3ba02835712ce03fbf8cfe872f9a0080dfdff
Olli-Pekka Huovilainen <ohuovila@gmail.com>
2010-06-03 21:48:40 +0000

    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pint@47285 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/initialize2.R
+++ R/initialize2.R
@@ -1,38 +1,37 @@
 initialize2 <-
-function (X,Y) {
+function (X, Y) {
 
+  #################################################
+  #  Initialization
+  #################################################
 
-	#################################################
-	# Initialize
-	#################################################
-
-	Nsamples = ncol(X)
-	
-	Dim = list()
-	Dim$X = nrow(X)
-	Dim$Y = nrow(Y)
-
-	nullmat = array(0,dim=c(Dim$X,Dim$Y))
-
-	Dcov = list()
-	Dcov$X = cov(t(X))
-	Dcov$Y = cov(t(Y))
-	Dcov$xy = cov(t(X),t(Y))
-	Dcov$yx = cov(t(Y),t(X))
-	Dcov$total = cov(t(rbind(X,Y)))
-	Dcov$sum = cov(t(X+Y))
-
-	# Initialize 
-	W = list()
-	W$X = W$Y = eigen(cov(t(X+Y)))$vectors
-	W$total = rbind(W$X,W$Y)
-
-	phi.init = list()
-	phi.init$X = Dcov$X
-	phi.init$Y = Dcov$Y
-        phi.init$total = rbind(cbind(phi.init$X,nullmat), cbind(nullmat, phi.init$Y))
-
-	list(phi=phi.init, W = W, Dcov=Dcov,Dim=Dim,nullmat=nullmat,Nsamples=Nsamples)
+  Nsamples <- ncol(X)
+  
+  Dim <- list()
+  Dim$X <- nrow(X)
+  Dim$Y <- nrow(Y)
+  
+  nullmat <- array(0, dim = c(Dim$X, Dim$Y))
+  
+  Dcov <- list()
+  Dcov$X <- cov(t(X), use = "pairwise.complete.obs")
+  Dcov$Y <- cov(t(Y), use = "pairwise.complete.obs")
+  Dcov$xy <- cov(t(X), t(Y), use = "pairwise.complete.obs")
+  Dcov$yx <- cov(t(Y), t(X), use = "pairwise.complete.obs")
+  Dcov$total <- cov(t(rbind(X,Y)))
+  Dcov$sum <- cov(t(X+Y), use = "pairwise.complete.obs")
+  
+  # Initialize 
+  W <- list()
+  W$X <- W$Y <- eigen(cov(t(X+Y), use = "pairwise.complete.obs"))$vectors
+  W$total <- rbind(W$X,W$Y)
+  
+  phi.init <- list()
+  phi.init$X <- Dcov$X
+  phi.init$Y <- Dcov$Y
+  phi.init$total <- rbind(cbind(phi.init$X,nullmat), cbind(nullmat, phi.init$Y))
+  
+  list(phi = phi.init, W = W, Dcov = Dcov, Dim = Dim, nullmat = nullmat, Nsamples = Nsamples)
 
 }
 
