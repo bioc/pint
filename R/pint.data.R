30de23017356eb2036fe6a5e5c17f4614675a599
Olli-Pekka Huovilainen <ohuovila@gmail.com>
2010-12-27 19:17:25 +0000

    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pint@51681 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/pint.data.R
+++ R/pint.data.R
@@ -1,57 +1,76 @@
-pint.data <- function(data, info, impute = TRUE, replace.inf = TRUE){
+pint.data <- function(data, info, impute = TRUE, replace.inf = TRUE, remove.duplicates = TRUE){
 
-  # Probe data
-  if (class(data) == "data.frame"){
-    data <- as.matrix(data)
+  data <- as.data.frame(data)
+  info <- as.data.frame(info)
+  
+  ## Replace synonymous info fields
+  # chromosome -> chr
+  if ("chromosome" %in% colnames(info) && !"chr" %in% colnames(info)) {
+    colnames(info)[which(colnames(info) == "chromosome")] <- "chr"
+  }
+  if ("stop" %in% colnames(info) && !"end" %in% colnames(info)) {
+    colnames(info)[which(colnames(info) == "stop")] <- "end"
   }
 
+  # If same number of rows and columns for data and info fields, 
+  # assume that they match between data and info fields and give both
+  # same rownames
+  if (nrow(data) == nrow(info) && !rownames(data) == rownames(info)) {
+     warning("The data and info fields had different rownames; using the rownames from data field for both.")
+     rownames(info) <- rownames(data)
+  }
 
+  # First, provide information in a form that has corresponding rows in
+  # data and info matrices (only take those available in both)
+  coms <- intersect(rownames(data), rownames(info))
+  coms <- setdiff(coms, c(""))
+  data <- data[coms,]
+  info <- info[coms,]
+  info[["chr"]] <- as.character(info[["chr"]])
+
+  # X/Y chromosome naming
+  if ("X" %in% info[["chr"]]) {
+    message("Changed chromosome name X to 23 for compatibility.")
+    info[info[["chr"]] == "X", "chr"] <- "23"
+  }
+  if ("Y" %in% info[["chr"]]) {
+    message("Changed chromosome name Y to 24 for compatibility.")
+    info[info[["chr"]] == "Y", "chr"] <- "24"
+  }
+
+  # Quarantee that there are no duplicated rows (probes) in the data
+  if (remove.duplicates){
+    dupl <- duplicated(data)
+    if (any(dupl)) {
+      message("Removing duplicate probe signals..\n")
+      data <- data[!dupl, ]
+      info <- info[!dupl, ]
+    }
+  }
+  
   # Impute missing values
   if (impute) {
     message("Imputing missing values..")
-    data <- impute(data)
+    data <- imputation(data)
   }
   
   # Replace infinite values by highest possible seen in the data
-  if (replace.inf) {
+  inds <- is.infinite(data)
+  if (replace.inf && any(inds)) {
      message("Replacing infinite values with highest non-infinite values seen in the data")
-     inds <- is.infinite(data)
      data[inds] <- sign(data[inds])*max(abs(data[!inds])) # note the sign
      message(paste("...", 100*mean(inds), "percent of the values replaced."))
   }
   
-  
-  ## Dealing with NaNs
-  ## variables (rows) with NaNs
-  #rows <- which(rowSums(is.nan(data)) > 0)
-  #if (length(rows) > 0) {
-  #  nans <- 0;
-  #  # Replace NaNs with samples from normal distribution
-  ##  for(i in rows) {
-  #    mean <- mean(data[i,],na.rm=TRUE)
-  ##    sd <- sd(data[i,],na.rm=TRUE)
-  ##    inds <- which(is.nan(data[i,]))
-  #    for(j in inds){
-  #        data[i,j] <- rnorm(1,mean,sd)
-  #        nans <- nans + 1
-  #    }
-  #  }
-  #  warning(paste(nans,"NaNs in probe data imputed with samples from normal distribution"))
-  #}
-  
-  
- 
   # Location
   if (is.null(info$loc) && is.null(info$bp)) {
     loc <- (info$start + info$end)/2
-  } 
-  else {
-    if (!is.null(info$bp))
-      loc <- info$bp
-    else
-      loc <- info$loc
+  } else if (!is.null(info$bp)) {
+    loc <- info$bp
+  } else {
+    loc <- info$loc
   }
-  
+ 
   # Arm
   if (is.null(info$arm)) {
     arm <- factor(rep("p",length(loc)),levels=c("p","q"))
@@ -60,12 +79,22 @@ pint.data <- function(data, info, impute = TRUE, replace.inf = TRUE){
     arm <- info$arm
   }
   
-  info <- data.frame(chr = factor(info$chr, levels = c(1:22,"X","Y")), arm = arm, loc = as.numeric(loc))
+  info <- data.frame(chr = factor(info$chr, levels = c(1:24)), arm = arm, loc = as.numeric(loc))
 
   # Order data by chr, arm and loc
   ord <- order(info$chr,info$arm,info$loc)
   data <- data[ord,]
   info <- info[ord,]
+
+
+  # If location information ('loc') for the probe is missing
+  # but start and end positions are available, use them to calculate
+  # probe middle bp location
+  if (!"loc" %in% colnames(info)) {
+    # sometimes factors, sometimes numerics given;
+    # this should handle both cases correctly
+    info[["loc"]] <- (as.numeric(as.character(info[, "start"])) + as.numeric(as.character(info[, "end"])))/2
+  }
 
   list(data = data, info = info)
 }
--- R/pint.match.R
+++ R/pint.match.R
@@ -1,90 +1,22 @@
-pint.match <- function(X, Y, max.dist = 1e7, chrs = NULL,
-useSegmentedData = FALSE, impute = TRUE, replace.inf = TRUE){
+pint.match <- function(X, Y, max.dist = 1e7, chrs = NULL, useSegmentedData = FALSE, impute = TRUE, replace.inf = TRUE){
 
-  if (all(is.na(X$data))) {stop("X data is empty/NA.")}
-  if (all(is.na(Y$data))) {stop("Y data is empty/NA.")}
-  
   # Match genes/probes/clones in X/Y data sets based on location information
-  #X <- geneExp; Y <- geneCopyNum
-
-  # If same number of rows and columns for data and info fields, 
-  # assume that they match between data and info fields and give both
-  # same rownames
-  if (nrow(X$data) == nrow(X$info) && !rownames(X$data) == rownames(X$info)) {
-     warning("The data and info fields in the X data set had different
-rownames; using the rownames from data field for both.")
-     rownames(X$info) <- rownames(X$data)
-  }
-  if (nrow(Y$data) == nrow(Y$info)) {
-     warning("The data and info fields in the Y data set had different
-rownames; using the rownames from data field for both.")  
-    rownames(Y$info) <- rownames(Y$data)
-  }
-
-  # First, provide information in a form that has corresponding rows in
-  # data and info matrices (only take those available in both)
-  coms <- intersect(rownames(X$data), rownames(X$info))
-  coms <- setdiff(coms, c(""))
-  X$data <- X$data[coms,]
-  X$info <- X$info[coms,]
-
-  coms <- intersect(rownames(Y$data), rownames(Y$info))
-  coms <- setdiff(coms, c(""))
-  Y$data <- Y$data[coms,]
-  Y$info <- Y$info[coms,]
-
-  X$info[["chr"]] <- as.character(X$info[["chr"]])
-  Y$info[["chr"]] <- as.character(Y$info[["chr"]])
-
-  # X/Y chromosome for data set X
-  if ("X" %in% X$info[["chr"]]) {X$info[X$info[["chr"]] == "X", "chr"] <- "23"}
-  if ("Y" %in% X$info[["chr"]]) {X$info[X$info[["chr"]] == "Y", "chr"] <- "24"}
-
-  # X/Y chromosome for data set X
-  if ("X" %in% Y$info[["chr"]]) {Y$info[Y$info[["chr"]] == "X", "chr"] <- "23"}
-  if ("Y" %in% Y$info[["chr"]]) {Y$info[Y$info[["chr"]] == "Y", "chr"] <- "24"}
-
-  # Quarantee that there are no duplicated rows (probes) in the data
-  if (!useSegmentedData){
-    dupl <- duplicated(X$data)
-    if (any(dupl)) {
-      cat("Removing duplicate probe signals on X data..\n")
-      X$data <- X$data[!dupl, ]
-      X$info <- X$info[!dupl, ]
-    }
-
-    dupl <- duplicated(Y$data)
-    if (any(dupl)) {
-      cat("Removing duplicate probe signals on Y data..\n")
-      Y$data <- Y$data[!dupl, ]
-      Y$info <- Y$info[!dupl, ]
-    }
-  }
-
-
-  # Impute missing values, replace infinite values etc.
-  X <- pint.data(X$data, X$info, impute, replace.inf)
-  Y <- pint.data(X$data, X$info, impute, replace.inf)
-
   
-  # First order chromosomes 1...22, X, Y, then chromosomes with other names
+  if (all(is.na(X$data))) { stop("X data is empty/NA.") }
+  if (all(is.na(Y$data))) { stop("Y data is empty/NA.") }
+  
+  # Impute missing values, replace infinite values etc.
+  X <- pint.data(X$data, X$info, impute, replace.inf, remove.duplicates = !useSegmentedData)
+  Y <- pint.data(Y$data, Y$info, impute, replace.inf, remove.duplicates = !useSegmentedData)
+
+  # Find unique chromosome names present in the data
+  # First order chromosomes 1...24, then chromosomes with other names
   if (is.null(chrs)) {
-    chrs <- c(as.character(1:24), sort(setdiff(unique(X$info[["chr"]]), as.character(1:24))))
+    default.chrs <- as.character(1:24)
+    chrs <- c(default.chrs, sort(setdiff(unique(X$info[["chr"]]), default.chrs)))
   } else {
     chrs <- as.character(chrs)
   }
-
-  # If location information ('loc') for the probe is missing
-  # but start and end positions are available, use them to calculate
-  # probe middle bp location
-  if (!"loc" %in% colnames(Y$info)) {
-    # sometimes factors, sometimes numerics given; this should handle both cases correctly
-    Y$info[["loc"]] <- (as.numeric(as.character(Y$info[, "start"])) + as.numeric(as.character(Y$info[, "end"])))/2
-  }
-  if (!"loc" %in% colnames(X$info)) {
-    X$info[["loc"]] <- (as.numeric(as.character(X$info[, "start"])) + as.numeric(as.character(X$info[, "end"])))/2
-  }
-  
 
   message("Matching probes between the data sets..")
   xindices <- yindices <- vector()
@@ -107,15 +39,11 @@ rownames; using the rownames from data field for both.")
     warning(paste("Samples ", colnames(X$data)[nainds], " contained exclusively NA's; removed."))
   }
   
-  # Convert chr to factor
-  if ("23" %in% X$info[["chr"]]) {X$info[X$info[["chr"]] == "23", "chr"] <- "X"}
-  if ("24" %in% X$info[["chr"]]) {X$info[X$info[["chr"]] == "24", "chr"] <- "Y"}
+  #X$info$chr = factor(X$info$chr, levels = c(1:22, "X", "Y"))
+  #Y$info$chr = factor(Y$info$chr, levels = c(1:22, "X", "Y"))
 
-  if ("23" %in% Y$info[["chr"]]) {Y$info[Y$info[["chr"]] == "23", "chr"] <- "X"}
-  if ("24" %in% Y$info[["chr"]]) {Y$info[Y$info[["chr"]] == "24", "chr"] <- "Y"}
-
-  X$info$chr = factor(X$info$chr, levels = c(1:22, "X", "Y"))
-  Y$info$chr = factor(Y$info$chr, levels = c(1:22, "X", "Y"))
+  X$info$chr = factor(X$info$chr, levels = c(1:24))
+  Y$info$chr = factor(Y$info$chr, levels = c(1:24))
 
   newX <- list(data = xdat, info = X$info[xindices,])
   newY <- list(data = ydat, info = Y$info[yindices,])
@@ -134,7 +62,6 @@ get.neighboring.probes <- function (X, Y, chr, max.dist, control.arms = TRUE) {
   
   # Use arm information if it is available and not blocked
   if (("arm" %in% names(X$info)) && ("arm" %in% names(Y$info)) && control.arms) {    
-
     for (arm in c('p', 'q')){      
       # Investigate specified arm
       xchrinds <- which(as.character(X$info$chr) == chr & X$info$arm == arm)
