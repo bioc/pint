25daf95623926e0cbd9ba0592f186336b8ddfb62
Olli-Pekka Huovilainen <ohuovila@gmail.com>
2011-03-02 20:11:45 +0000

    bibtex fixed
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pint@53367 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/firstlib.R
+++ R/firstlib.R
@@ -2,6 +2,7 @@
 {
    cat('\npint Copyright (C) 2008-2011 Olli-Pekka Huovilainen and Leo Lahti.\n
 This program comes with ABSOLUTELY NO WARRANTY.\n
-This is free software, and you are welcome to redistribute it under the FreeBSD license.\n
+This is free software, and you are welcome to redistribute it\n
+under the FreeBSD license.\n
 See the licensing terms for details.\n\n')
 }
